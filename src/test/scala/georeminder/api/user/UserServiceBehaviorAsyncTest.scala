d361af6ebf2ea80867c4106bb899edfd1a76bcdc
Andrey Spiridonov <spirtik87@gmail.com>
2020-06-25 13:02:31 +0200

    Added demo unit test for typed akka persistence

--- src/test/scala/georeminder/api/user/UserServiceBehaviorAsyncTest.scala
+++ src/test/scala/georeminder/api/user/UserServiceBehaviorAsyncTest.scala
@@ -1,24 +1,27 @@
 package georeminder.api.user
 
-import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import com.typesafe.config.ConfigFactory
+import georeminder.api.user.UserServiceBehavior.OperationResult
 import org.junit.runner.RunWith
-import org.scalatest.BeforeAndAfterAll
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatestplus.junit.JUnitRunner
+
+object TestUserBehaviorConfig {
+  val config = ConfigFactory.parseString(
+    """
+  akka.loglevel = DEBUG
+
+  """).withFallback(ConfigFactory.load())
+}
 
 @RunWith(classOf[JUnitRunner])
 class UserServiceBehaviorAsyncTest
-  extends AnyFunSuite
-    with BeforeAndAfterAll
-    with Matchers {
-
-  val testKit = ActorTestKit()
+  extends ScalaTestWithActorTestKit(TestUserBehaviorConfig.config) with AnyFunSuiteLike  {
 
   test("Registration of the user returns confirmation") {
     val userService = testKit.spawn(UserServiceBehavior(), "usb")
-    val probe = testKit.createTestProbe[UserServiceBehavior.RegisteredUser]()
+    val probe = testKit.createTestProbe[OperationResult]()
     userService ! UserServiceBehavior.RegisterUser("hello", probe.ref)
     probe.expectMessage(UserServiceBehavior.RegisteredUser())
   }
