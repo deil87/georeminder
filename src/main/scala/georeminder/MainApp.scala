c87a743dc7eea286dddf6c42b77dae859859fd3c
Andrey Spiridonov <spirtik87@gmail.com>
2020-07-03 20:26:58 +0200

    Added static files route

--- src/main/scala/georeminder/MainApp.scala
+++ src/main/scala/georeminder/MainApp.scala
@@ -2,13 +2,20 @@ package georeminder
 
 import akka.actor
 import akka.actor.ActorSystem
+import cats.data.Kleisli
 import cats.effect.ExitCase.Canceled
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{Blocker, ExitCode, IO, IOApp, Resource}
 import georeminder.api.user.UserService
 import georeminder.api.{HelloWorldService, TweetService}
-import org.http4s.implicits._
-import org.http4s.server.Router
+import org.http4s.dsl.impl.Root
+import org.http4s.{HttpApp, HttpRoutes, Request, Response}
+import org.http4s.server.{Router, Server}
 import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.staticcontent._
+import org.http4s.dsl.io._
+import org.http4s.implicits._ // this is being used for '.orNotFound'
+
+import scala.concurrent.ExecutionContext.global
 
 
 object MainApp extends IOApp {
@@ -20,24 +27,46 @@ object MainApp extends IOApp {
 //  val system: ActorSystem[HelloWorldMain.SayHello] =
 //    ActorSystem(HelloWorldMain(), "hello")
 
-
-  val httpApp = Router("/" -> helloWorldService.routes, "/api" -> tweetService.routes).orNotFound
+  def httpApp(blocker: Blocker):HttpApp[IO] = {
+    val value: HttpRoutes[IO] = fileService(FileService.Config("./assets", blocker))
+    val tweets: HttpRoutes[IO] = tweetService.routes
+    val router: HttpRoutes[IO] = Router(
+      "/" -> value,
+      "/api" -> tweets
+    )
+    router.orNotFound
+  }
 
   def run(args: List[String]): IO[ExitCode] = {
 
-    val program: IO[ExitCode] = BlazeServerBuilder[IO]
-      .bindHttp(8080, "0.0.0.0")
-      .withHttpApp(httpApp)
-      .serve
-      .compile
-      .drain
-      .as(ExitCode.Success)
+    val program = for {
+      blocker <- Blocker[IO]
+//      server <- BlazeServerBuilder[IO](global)
+//        .bindHttp(8080, "0.0.0.0")
+//        .withHttpApp(httpApp(blocker))
+//        .resource
 
-    program.guaranteeCase{
-      case Canceled =>
-        IO(println("Interrupted: releasing and exiting!"))
-      case _ =>
-        IO(println("Normal exit!"))
+    } yield {
+      val res = BlazeServerBuilder[IO](global)
+        .bindHttp(8080, "0.0.0.0")
+        .withHttpApp(httpApp(blocker))
+        .serve
+        .compile
+        .drain
+        .as(ExitCode.Success)
+
+      res
     }
+
+    val value: IO[ExitCode] = program.use(io => io)
+    value
+
+//    program.guaranteeCase{
+//      case Canceled =>
+//        IO(println("Interrupted: releasing and exiting!"))
+//      case _ =>
+//        IO(println("Normal exit!"))
+//    }
+
   }
 }
